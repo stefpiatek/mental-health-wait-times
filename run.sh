1364ce6bc58a272204d856d5dd3b9323013765c9
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-13 13:26:22 +0100

    Try adding install to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 set -euxo pipefail
 
+python3 -m pip install requirements.txt
 python3 manage.py migrate
 python3 manage.py loaddata data/test_data.json
 python3 -m gunicorn --workers 2 mental_health_wait_times.wsgi
