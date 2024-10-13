de337766ffb21fe36b94cad4f88e40e1d98959ae
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-13 10:36:17 +0100

    Use routine instead of non-urgent

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,16 +5,16 @@ def dashboard_view(request):
     # Example hardcoded data for actual wait times
     actual_wait_times = {
         'urgent': [120, 450, 222, 160, 140],
-        'non_urgent': [300, 600, 400, 200, 250]
+        'routine': [300, 600, 400, 200, 250]
     }
 
 
     # Calculate average wait times
     avg_actual_urgent = sum(actual_wait_times['urgent']) / len(actual_wait_times['urgent'])
-    avg_actual_non_urgent = sum(actual_wait_times['non_urgent']) / len(actual_wait_times['non_urgent'])
+    avg_actual_routine = sum(actual_wait_times['routine']) / len(actual_wait_times['routine'])
 
     context = {
         'avg_actual_urgent': avg_actual_urgent,
-        'avg_actual_non_urgent': avg_actual_non_urgent,
+        'avg_actual_routine': avg_actual_routine,
     }
     return render(request, 'dashboard/dashboard.html', context)
