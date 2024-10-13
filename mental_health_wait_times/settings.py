13d2b49a3be46dcd47e5e915e7d2ddd460f28ccc
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-13 12:16:48 +0100

    Add all allowed hosts

--- mental_health_wait_times/settings.py
+++ mental_health_wait_times/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--2z(grglg12fr16-_xh0oxn0qlgrahdwf&!q4d5zjhyre-md3$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
