d92f1dfea25cb9138ad4ad4e86d4e3566de09352
Philip Davidson <philip.davidson2@nhs.net>
2024-10-13 10:54:42 +0100

    added navbar, summary pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import dashboard_view
+from .views import dashboard_view, summary_view, all_patients_view
 
 urlpatterns = [
-    path('', dashboard_view, name='dashboard'),
+    path("", dashboard_view, name="dashboard"),
+    path("summary", summary_view, name="summary"),
+    path("all_patients", all_patients_view, name="all_patients"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from .models import Pathway
 
 
 def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
+    return render(request, "dashboard/dashboard.html")
+
+
+def summary_view(request):
+
+    pathway_data = Pathway.objects.all()
+
+    mh_waiting = (
+        pathway_data.filter(pathway_type__pathway_type="Mental Health")
+        .filter(first_contact_date__isnull=True)
+        .count()
+    )
+    nd_waiting = pathway_data.filter(
+        pathway_type__pathway_type="Neurodiversity"
+    ).count()
+
+    context = {"mh_waiting": mh_waiting, "nd_waiting": nd_waiting}
+
+    return render(request, "dashboard/summary.html", context=context)
+
+
+def all_patients_view(request):
+
+    pathway_data = Pathway.objects.all()
+
+    context = {"pathway_data": pathway_data}
+
+    return render(request, "dashboard/all_patients.html", context=context)
