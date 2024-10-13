b6ae30b9d58350c257ceae66fca8bd2e25aa4e82
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-13 09:34:11 +0100

    Update chart with sliderds

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+
 
 def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
+    # Example hardcoded data for actual wait times
+    actual_wait_times = {
+        'urgent': [30, 45, 50, 60, 40],
+        'non_urgent': [60, 70, 80, 90, 75]
+    }
+
+
+    # Calculate average wait times
+    avg_actual_urgent = sum(actual_wait_times['urgent']) / len(actual_wait_times['urgent'])
+    avg_actual_non_urgent = sum(actual_wait_times['non_urgent']) / len(actual_wait_times['non_urgent'])
+
+    context = {
+        'avg_actual_urgent': avg_actual_urgent,
+        'avg_actual_non_urgent': avg_actual_non_urgent,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
